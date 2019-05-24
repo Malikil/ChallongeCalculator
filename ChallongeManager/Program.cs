7dfba4ee10cedd4e68315aa0220f13dc7b5195b4
Malikil <eboy.coltt5@gmail.com>
2019-05-24 12:03:32 -0700

    Add project files.

---
+++ ChallongeManager/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ChallongeManager
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
+                    response.EnsureSuccessStatusCode();
+                    string body = await response.Content.ReadAsStringAsync();
+
+                    // string alternatebody = await client.GetStringAsync("https://jsonplaceholder.typicode.com/todos/1");
+                    Console.WriteLine(body);
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Oops, {ex.Message}");
+                }
+            }
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
